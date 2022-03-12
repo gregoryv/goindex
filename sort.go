891deafb50dd2d7181432768253f9dfe54d30f61
Gregory Vincic <g@7de.se>
2022-03-12 21:59:57 +0100

    FindConstructors

--- sort.go
+++ sort.go
@@ -5,6 +5,66 @@ import (
 	"go/token"
 )
 
+// FindConstructors returns a list of constructor functions for the
+// given type name
+func FindConstructors(src []byte, typeName string) []string {
+	var s scanner.Scanner
+	fset := token.NewFileSet()
+	file := fset.AddFile("", fset.Base(), len(src))
+	s.Init(file, src, nil /* no error handler */, 0)
+
+	result := make([]string, 0)
+loop:
+	for {
+		_, tok, _ := s.Scan()
+		switch tok {
+		case token.EOF:
+			break loop
+		case token.FUNC:
+			_, tok, name := s.Scan()
+			if tok == token.LPAREN { // ie. method
+				continue loop
+			}
+
+			// move to end of arguments
+			moveToReturns(s)
+			// parse return args
+		inner:
+			for {
+				_, tok, lit := s.Scan()
+				switch tok {
+				case token.EOF, token.LBRACE:
+					break inner
+				case token.IDENT:
+					if lit == typeName {
+						result = append(result, name)
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+
+func moveToReturns(s scanner.Scanner) {
+	var left int
+loop:
+	for {
+		_, tok, _ := s.Scan()
+		switch tok {
+		case token.LPAREN:
+			left++
+
+		case token.RPAREN:
+			left--
+			if left == 0 {
+				break loop
+			}
+		}
+	}
+
+}
+
 // FindTypes returns all named types in the given Go source
 func FindTypes(src []byte) []string {
 	var s scanner.Scanner
@@ -13,11 +73,12 @@ func FindTypes(src []byte) []string {
 	s.Init(file, src, nil /* no error handler */, 0)
 
 	result := make([]string, 0)
+loop:
 	for {
 		_, tok, _ := s.Scan()
 		switch tok {
 		case token.EOF:
-			break
+			break loop
 		case token.TYPE:
 			_, _, name := s.Scan()
 			result = append(result, name)
--- sort_test.go
+++ sort_test.go
@@ -5,18 +5,30 @@ import (
 	"log"
 )
 
-func init() {
-	log.SetFlags(0)
+func Example_FindConstructors() {
+	src := []byte(`package x
+type X struct {}
+func play() {}
+func New() *X { return &X{} }
+func Parse(v []byte) (*X, error) { return &X{}, nil }
+func (c *X) Clone(_ interface{ Y() }) *X { t := *c; return &t}
+`)
+
+	fmt.Print(FindConstructors(src, "X"))
+	// output:
+	// [New Parse]
 }
 
 func Example_FindTypes() {
 	src := []byte(`package x
-
 type X struct {}
+func NewX() *X { return &X{} }`)
 
-func NewX() *X { return &X{} }
-`)
 	fmt.Print(FindTypes(src))
 	// output:
 	// [X]
+}
+
+func init() {
+	log.SetFlags(0)
 }

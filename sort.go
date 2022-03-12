8101a7830503bd88ec3cac0e7784ac12e7ee3528
Gregory Vincic <g@7de.se>
2022-03-12 22:22:39 +0100

    Add Index

--- sort.go
+++ sort.go
@@ -5,6 +5,59 @@ import (
 	"go/token"
 )
 
+func Index(src []byte) []int {
+	var s scanner.Scanner
+	fset := token.NewFileSet()
+	file := fset.AddFile("", fset.Base(), len(src))
+	s.Init(file, src, nil /* no error handler */, scanner.ScanComments)
+
+	var index []int
+
+	for {
+		pos, tok, lit := s.Scan()
+		if tok == token.EOF {
+			break
+		}
+		j := fset.Position(pos).Offset
+
+		switch tok {
+		case token.STRING, token.IDENT, token.COMMENT:
+			j = fset.Position(pos).Offset + len(lit)
+		case token.TYPE, token.FUNC:
+		default:
+			j = fset.Position(pos).Offset + len(tok.String())
+		}
+
+		//log.Println("i:", i, "j:", j)
+		switch tok {
+		case token.TYPE, token.FUNC:
+			// add start of
+			index = append(index, j)
+			index = append(index, moveToEnd(fset, s))
+		}
+	}
+	return index
+}
+
+func moveToEnd(fset *token.FileSet, s scanner.Scanner) int {
+	var left int
+
+	for {
+		pos, tok, _ := s.Scan()
+		switch tok {
+		case token.LBRACE:
+			left++
+
+		case token.RBRACE:
+			left--
+			if left == 0 {
+				return fset.Position(pos).Offset + len(tok.String())
+
+			}
+		}
+	}
+}
+
 // FindConstructors returns a list of constructor functions for the
 // given type name
 func FindConstructors(src []byte, typeName string) []string {
@@ -62,7 +115,6 @@ loop:
 			}
 		}
 	}
-
 }
 
 // FindTypes returns all named types in the given Go source
--- sort_test.go
+++ sort_test.go
@@ -1,13 +1,39 @@
 package gosort
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"testing"
 )
+
+func Test_Index(t *testing.T) {
+	src := []byte(`package x
+
+// X stores info
+type X struct {
+}
+
+// not much here
+
+func play() {
+_ = "hey"
+}`)
+	var from int
+	var buf bytes.Buffer
+	for _, to := range Index(src) {
+		fmt.Fprint(&buf, string(src[from:to]))
+		from = to
+	}
+	if got := buf.String(); got != string(src) {
+		t.Error(got)
+	}
+}
 
 func Example_FindConstructors() {
 	src := []byte(`package x
-type X struct {}
+type X struct {
+}
 func play() {}
 func New() *X { return &X{} }
 func Parse(v []byte) (*X, error) { return &X{}, nil }

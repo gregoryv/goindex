bf878f76a6a0d4ddbd1b0d7b63455e0711f82a06
Gregory Vincic <g@7de.se>
2022-03-29 13:12:42 +0200

    Add type GoFile

--- gofile.go
+++ gofile.go
@@ -1,6 +1,31 @@
 package gosort
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
+
+type GoFile struct {
+	sections []Section
+	src      []byte
+}
+
+func (me *GoFile) WriteTo(w io.Writer) (int, error) {
+	var last int
+	var total int
+	for _, s := range me.sections {
+		n, err := w.Write(me.src[last:s.Position()])
+		if err != nil {
+			return total + n, err
+		}
+		total += n
+		last = s.Position()
+	}
+	n, err := w.Write(me.src[last:]) // write final
+	return total + n, err
+}
+
+// ----------------------------------------
 
 type Section struct {
 	position int

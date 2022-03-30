2fcac5c6dbcdd2902fb9938dc03231409cf4214d
Gregory Vincic <g@7de.se>
2022-03-30 12:01:40 +0200

    Add cmd/goindex

---
+++ cmd/goindex/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gregoryv/goindex"
+)
+
+func main() {
+	flag.Parse()
+	filename := flag.Arg(0)
+
+	log.SetFlags(0)
+
+	src, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sections := goindex.Index(src)
+	for _, s := range sections {
+		fmt.Printf("%s %v %v\n", s.Decl(), s.From(), s.To())
+	}
+}

568ac4b21b0210a934a0ab03642662d60fef96f8
Gregory Vincic <gregoryvse@gmail.com>
2023-12-17 12:55:30 +0100

    Add flag -verbose to cmd/index

--- cmd/index/main.go
+++ cmd/index/main.go
@@ -1,10 +1,11 @@
-// Command goindex indexes a go source file into sections
+// Command index indexes source files into sections
 package main
 
 import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/gregoryv/goindex"
 )
@@ -14,17 +15,29 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Usage: %s FILES...\n", os.Args[0])
 		flag.PrintDefaults()
 	}
+	verbose := flag.Bool("verbose", false, "progress to stderr")
 	flag.Parse()
 
 	for _, filename := range flag.Args() {
-		src, err := os.ReadFile(filename)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		sections := goindex.Index(src)
-		for _, s := range sections {
-			fmt.Printf("%s %s\n", filename, s.String())
+		ext := filepath.Ext(filename)
+		switch ext  {
+		case ".go":
+			if *verbose {
+				fmt.Fprintln(os.Stderr, "parse", filename, ext)
+			}
+			src, err := os.ReadFile(filename)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			sections := goindex.Index(src)
+			for _, s := range sections {
+				fmt.Printf("%s %s\n", filename, s.String())
+			}
+		default:
+			if *verbose {
+				fmt.Fprintln(os.Stderr, "skip", filename, ext)
+			}
 		}
 	}
 }
